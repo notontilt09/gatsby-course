7333040e19fe72a10f48f0b3817c66797cc8318a
Daniel Weinman <namniewd@gmail.com>
2020-10-20 18:11:29 -0400

    initial commit, removed starter and finished files and begun gatsby tooling setup

---
+++ gatsby/src/components/Nav.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+  background: red;
+`;
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Hot Now</Link>
+        </li>
+        <li>
+          <Link to="/pizzas">Pizza Menu</Link>
+        </li>
+        <li>
+          <Link to="/">Logo</Link>
+        </li>
+        <li>
+          <Link to="/slicemasters">SliceMasters</Link>
+        </li>
+        <li>
+          <Link to="/order">Order Ahead! </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
