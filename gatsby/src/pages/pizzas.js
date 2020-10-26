30a07d5a89096b8a6809c4cf02cce74ae1233c48
Daniel Weinman <namniewd@gmail.com>
2020-10-25 21:44:14 -0400

    finished module 6, added sample data and begun styling pizzas and topping filter with css grid

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PizzaList from '../components/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({ data }) {
   const pizzas = data.pizzas.nodes;
   return (
     <>
+      <ToppingsFilter />
       <PizzaList pizzas={pizzas} />
     </>
   );
@@ -13,6 +15,7 @@ export default function PizzasPage({ data }) {
 
 export const query = graphql`
   query PizzaQuery {
+    # renames allSanityPizza query to pizzas query (when viewed in dev tools)
     pizzas: allSanityPizza {
       nodes {
         name
