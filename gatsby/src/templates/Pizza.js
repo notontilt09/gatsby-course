d93b89a9562f3b228792c1fed7a682c6a7aeeee7
Daniel Weinman <namniewd@gmail.com>
2020-11-01 18:44:48 -0500

    thru video 33, created single slicemaster pages and added SEO component to each page

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
+import SEO from '../components/SEO';
 
 const PizzaGrid = styled.div`
   display: grid;
@@ -12,17 +14,23 @@ const PizzaGrid = styled.div`
 export default function SinglePizzaPage({ data }) {
   const { pizza } = data;
   return (
-    <PizzaGrid>
-      <Img fluid={pizza.image.asset.fluid} />
-      <div>
-        <h2 className="mark">{pizza.name}</h2>
-        <ul>
-          {pizza.toppings.map((topping) => (
-            <li key={topping.id}>{topping.name}</li>
-          ))}
-        </ul>
-      </div>
-    </PizzaGrid>
+    <>
+      <SEO title={pizza.name} image={pizza.image?.asset?.fluid?.src} />
+      <PizzaGrid>
+        <Helmet>
+          <title>{pizza.name}</title>
+        </Helmet>
+        <Img fluid={pizza.image.asset.fluid} />
+        <div>
+          <h2 className="mark">{pizza.name}</h2>
+          <ul>
+            {pizza.toppings.map((topping) => (
+              <li key={topping.id}>{topping.name}</li>
+            ))}
+          </ul>
+        </div>
+      </PizzaGrid>
+    </>
   );
 }
 // Needs to be dynamic based on the slub passed in via gatsby-node
